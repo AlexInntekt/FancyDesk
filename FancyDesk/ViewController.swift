16292dd947a5a74b2b35197fd1fdd3a52a2b6c09
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-02-12 14:35:45 +0200

    Initial Commit

---
+++ FancyDesk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FancyDesk
+//
+//  Created by Manolescu Mihai Alexandru on 12/02/2018.
+//  Copyright © 2018 Manolescu Mihai Alexandru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
