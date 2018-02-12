168935afaa47d8073d663ce09d11b6f09241e6b6
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-02-12 15:35:01 +0200

    First commit. The app has only a starting button that animate the resizing of the top bar.

--- FancyDesk/ViewController.swift
+++ FancyDesk/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet var headerViewHeight: NSLayoutConstraint!
+    
+ 
+    
+    
+    @IBOutlet var headerView: UIView!
+    
+    @IBOutlet var viewX: UIViewX!
+    
+    
+    @IBOutlet var startButton: UIButton!
+    @IBAction func startButton(_ sender: Any)
+    {
+        animateStart()
+    }
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   func animateStart()
+   {
+    self.view.layoutIfNeeded()
+    
+    //change the constraint and only then we can animate the change:
+    //self.headerViewHeight.constant = 100
+    
+       UIView.animate(withDuration: 1.5)
+       {
+        self.headerView.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 4)
+        
+        self.viewX.transform = CGAffineTransform.identity.translatedBy(x: -4*self.view.frame.width, y: 0)
+       }
+    
+    
+    
     }
-
 
 }
 
