98c34ea5b4bba9608eb837def048c781deef5bdf
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-02-15 00:11:42 +0200

    I added a nice scrollView with images.

--- FancyDesk/ViewController.swift
+++ FancyDesk/ViewController.swift
@@ -17,7 +17,12 @@ class ViewController: UIViewController
     
     @IBOutlet var viewX: UIViewX!
     
+    @IBOutlet var scrollView: UIScrollView!
+    
+    
     var imageView = UIImageView()
+    
+    var imagesForScrollView = [UIImageView]()
     
     var animationContainerView: UIView!
     
@@ -33,19 +38,63 @@ class ViewController: UIViewController
         super.viewDidLoad()
       
         
-        
     }
 
     override func viewDidAppear(_ animated: Bool)
     {
         super.viewDidAppear(animated)
         
+        
+        
+        scrollView.layer.cornerRadius = 20
+        
+        var contentWidth: CGFloat = 0.0
+        
+        for x in 0...2
+        {
+            let image = UIImage(named: "wood\(x).jpg")
+            let imageView = UIImageView(image: image)
+            imagesForScrollView.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = CGFloat(0.8) * view.frame.size.width * CGFloat(x)
+            
+            contentWidth += scrollView.frame.width
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX, y: 0, width: scrollView.frame.width, height: scrollView.frame.height)
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: scrollView.frame.size.height)
+        
+//        var contentWidth: CGFloat = 0.0
+//
+//        for x in 0...2
+//        {
+//            let image = UIImage(named: "wood\(x).jpg")
+//            let imageView = UIImageView(image: image)
+//            imagesForScrollView.append(imageView)
+//
+//            var newX: CGFloat = 0.0
+//
+//            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+//
+//            contentWidth += newX
+//
+//            scrollView.addSubview(imageView)
+//
+//            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 300, height: 300)
+//        }
+//
+//        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
     }
     
    func animateStart()
    {
-    self.headerView.transform = CGAffineTransform.identity
-  
+
     UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 0, initialSpringVelocity: 0, options: [.curveEaseIn], animations: {self.headerView.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 5)})
     
 
@@ -55,27 +104,27 @@ class ViewController: UIViewController
     imageView.layer.masksToBounds = true
     imageView.translatesAutoresizingMaskIntoConstraints = false
     
-    view.addSubview(imageView)
-    
-    let conX = imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-
-    let conBottom = imageView.bottomAnchor.constraint(equalTo:
-        view.bottomAnchor, constant: imageView.frame.height/3)
- 
-    let conWidth = imageView.widthAnchor.constraint(equalTo:
-        view.widthAnchor, multiplier: 0.6, constant: 0)
-    
-    let conHeight = imageView.heightAnchor.constraint(equalTo:
-        imageView.widthAnchor)
-    
-    NSLayoutConstraint.activate([conX,conBottom,conWidth,conHeight])
-    
-    view.layoutIfNeeded()
+//    view.addSubview(imageView)
+//
+//    let conX = imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+//
+//    let conBottom = imageView.bottomAnchor.constraint(equalTo:
+//        view.bottomAnchor, constant: imageView.frame.height/3)
+//
+//    let conWidth = imageView.widthAnchor.constraint(equalTo:
+//        view.widthAnchor, multiplier: 0.6, constant: 0)
+//
+//    let conHeight = imageView.heightAnchor.constraint(equalTo:
+//        imageView.widthAnchor)
+//
+//    NSLayoutConstraint.activate([conX,conBottom,conWidth,conHeight])
+//
+//    view.layoutIfNeeded()
     
     
     UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0, options: [.curveEaseInOut] ,animations:
     {
-        conBottom.constant = -300
+        //conBottom.constant = -300
         
         self.view.layoutIfNeeded()
     })
